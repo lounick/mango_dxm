ee9b80274517449234c4496aed67770f27fede33
Nick Tsiogkas <nt95@hw.ac.uk>
2016-09-04 23:24:57 +0100

    Added method to insert to db. modified item classes and message structure to accomodate ack messages.

--- scripts/dxm.py
+++ scripts/dxm.py
@@ -14,19 +14,20 @@ import struct
 import roslib
 roslib.load_manifest('mango_dxm')
 import rospy
-from std_msgs.msg import String
 from sunset_ros_networking_msgs.msg import SunsetTransmission, SunsetReception, SunsetNotification
 from mango_dxm.srv import *
-import json
 
-class vehicle_info:
+
+
+class VehicleInfo:
     type_id_ = 1
     status_shift_ = 4
     status_mask_ = 240
     intention_mask_ = 15
     pack_fmt_ = 'BQdddBBL'
-    def __init__(self, id, lat, lon, depth, status, intention, timestamp):
-        self.id = id
+
+    def __init__(self, uid, lat, lon, depth, status, intention, timestamp):
+        self.id = uid
         self.lat = lat
         self.lon = lon
         self.depth = depth
@@ -38,17 +39,18 @@ class vehicle_info:
         # vinfo = 0
         # vinfo |= self.status_ << self.status_shift_
         # vinfo |= self.intention_
-        return struct.pack(self.pack_fmt_, self.type_id_, self.uid_, self.lat_, self.lon_, self.depth_, self.status_, self.intention_, self.timestamp_)
+        return struct.pack(self.pack_fmt_, self.type_id_, self.id, self.lat, self.lon, self.depth, self.status, self.intention, self.timestamp)
 
 
-class target_info:
+class TargetInfo:
     type_id_ = 2
     vehicle_shift_ = 4
     vehicle_mask_ = 240
     classification_mask_ = 15
     pack_fmt_ = 'BQdddBBL'
-    def __init__(self, id, lat, lon, depth, vehicle_id, classification, timestamp):
-        self.id = id
+
+    def __init__(self, uid, lat, lon, depth, vehicle_id, classification, timestamp):
+        self.id = uid
         self.lat = lat
         self.lon = lon
         self.depth = depth
@@ -60,14 +62,13 @@ class target_info:
         # tinfo = 0
         # tinfo |= self.vehicle_id_ << self.vehicle_shift_
         # tinfo |= self.classification_
-        return struct.pack(self.pack_fmt_, self.type_id_, self.uid_, self.lat_, self.lon_, self.depth_, self.vehicle_id_, self.classification_, self.timestamp_)
-
-
+        return struct.pack(self.pack_fmt_, self.type_id_, self.id, self.lat, self.lon, self.depth, self.vehicle_id, self.classification, self.timestamp)
 
 
 class Dxm:
     def __init__(self, module_id, port):
         self.msg_flag_ = False
+        self.msg_count_ = 0
         self.module_id_ = module_id
         self.msg_pub_ = rospy.Publisher('sunset_networking/sunset_transmit_'+self.module_id_, SunsetTransmission, queue_size=10)
         self.msg_rec_sub_ = rospy.Subscriber('sunset_networking/sunset_reception_'+self.module_id_, SunsetReception, self.received_cb)
@@ -80,6 +81,8 @@ class Dxm:
         # r.set_loop_type("tornado")
         # self.connection = r.connect(host='localhost', port=28015)
         self.connection_ = None
+        self.last_inserted_id_ = None
+        self.last_timestamp_ = None
         self.db_ready_ = False
 
     def init_db(self):
@@ -104,16 +107,26 @@ class Dxm:
     def received_cb(self, msg):
         # You received a message, get the payload, decode and decide what to do
         # Send an ack
-        pack_fmt_ = 'BQdddBBL'
-        (type_id, uid, lat_, lon, depth, info1, info2, timestamp) = struct.unpack(pack_fmt_, msg.payload)
+        msg_pack_fmt_ = 'QdddBBL'
+        msg_id = struct.unpack('L', msg.payload[0:4])
+        type_id = struct.unpack('B', msg.payload[4])
         if type_id == 1:
-            vehicle = vehicle_info(type_id, uid, lat_, lon, depth, info1, info2, timestamp)
+            (uid, lat_, lon, depth, info1, info2, timestamp) = struct.unpack(msg_pack_fmt_, msg.payload[5:])
+            vehicle = VehicleInfo(uid, lat_, lon, depth, info1, info2, timestamp)
+            self.insert_db("vehicles", vehicle)
+            # TODO: Create an ack and send it
         elif type_id == 2:
-            target = target_info(type_id, uid, lat_, lon, depth, info1, info2, timestamp)
-
+            (uid, lat_, lon, depth, info1, info2, timestamp) = struct.unpack(msg_pack_fmt_, msg.payload[5:])
+            target = TargetInfo(uid, lat_, lon, depth, info1, info2, timestamp)
+            self.insert_db("targets", target)
+            # TODO: Create an ack and send it
+        elif type_id == 3:
+            # It is an ack for a message we sent. Add this to acked messages.
+            acked_msg_id = struct.unpack('L', msg.payload[5:])
+            # TODO: Populate acked message list for message
 
     def notification_cb(self, msg):
-        # Got a notiffication from sunset. Deal with that
+        # Got a notification from sunset. Deal with that
         if msg.notification_type == 1:
             if msg.notification_subtype == 1:
                 rospy.loginfo("Transmission started")
@@ -141,15 +154,34 @@ class Dxm:
     def gen_uid(self):
         pass
 
-    def insert_db(self, type, item):
-        pass
+    def insert_db(self, table, item):
+        tmp_id = self.last_inserted_id_
+        tmp_ts = self.last_timestamp_
+        self.last_inserted_id_ = item.id
+        self.last_timestamp_ = item.timestamp
+        if r.table(table).get(item.id).run(self.connection_) is None:
+            # This vehicle doesn't exist. Insert into DB.
+            res = r.table(table).insert(item.__dict__).run(self.connection_)
+            if res['errors'] != 0:
+                self.last_inserted_id_ = tmp_id
+                self.last_timestamp_ = tmp_ts
+                rospy.logerr("Error inserting in table: " + table + ". With error: " + res['first_error'])
+        else:
+            # Vehicle exists in DB. Just update its info.
+            res = r.table(table).update(item.__dict__).run(self.connection_)
+            if res['errors'] != 0:
+                self.last_inserted_id_ = tmp_id
+                self.last_timestamp_ = tmp_ts
+                rospy.logerr("Error updating in table: " + table + ". With error: " + res['first_error'])
 
     @gen.coroutine
     def rethink_vehicle_cb(self):
+        # We have a vehicle update. Queue it for transmission
         pass
 
     @gen.coroutine
     def rethink_target_cb(self):
+        # We have a target update. Queue it for transmission
         pass
 
     @gen.coroutine
@@ -163,7 +195,7 @@ class Dxm:
 
 def main(argv):
     module_id = argv[0]
-    if (len(argv) > 1):
+    if len(argv) > 1:
         port = argv[1]
     else:
         port = 9000 + int(module_id)
@@ -177,7 +209,7 @@ def main(argv):
         pass
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 2):
+    if len(sys.argv) < 2:
         print "Usage: dxm moduleId (port)"
     else:
         main(sys.argv[1:])
